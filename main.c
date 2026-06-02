ba43097c70474751cbbfa360e58cc32fbf6975e1
progra <progra@progra>
2026-06-02 19:37:54 -0400

    prueba2

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 
 int suma_digitos(int n1)
 {
-return ((n1 % 10) + digitos(n1 / 10)); //caso recursivo
+	if (n1/10==0)
+	{
+		return n1
+	}
+	else
+	{
+		return ((n1 % 10) + digitos(n1 / 10)); //caso recursivo
+	}
+
 }
